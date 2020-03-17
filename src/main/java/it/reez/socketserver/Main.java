d52c54452210dbf190bf1665b36a9025b0be34d0
ReeZey <unconfigured@null.spigotmc.org>
2020-03-17 19:56:57 +0100

    added browser support

--- src/main/java/it/reez/socketserver/Main.java
+++ src/main/java/it/reez/socketserver/Main.java
@@ -3,7 +3,9 @@ package it.reez.socketserver;
 import java.io.*;
 import java.net.*;
 
+import java.nio.channels.SocketChannel;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,7 +16,7 @@ public class Main {
 
         int port = 1234;
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        try (ServerSocket server = new ServerSocket(port)) {
             System.out.println("Server is listening on port " + port);
             Mars.load();
             Map<String, Rover> players = new HashMap<>();
@@ -23,88 +25,106 @@ public class Main {
             players.put("5010", new Rover(24, 4, 3));
 
             while (true) {
-                Socket socket = serverSocket.accept();
-                OutputStream output = socket.getOutputStream();
-                PrintWriter writer = new PrintWriter(output);
+                Socket socket = server.accept();
 
-                writer.println("Connected");
-                writer.flush();
+                InetAddress line = socket.getInetAddress();
+                if(line.toString().equals("/127.0.0.1")){
+                    StringBuilder html = new StringBuilder();
+                    for(Map.Entry<String, Rover> rov : players.entrySet()) {
+                        Rover r = rov.getValue();
+                        String pos = "\""+rov.getKey()+"\":{\"x\":\""+r.getX() + "\", \"y\": \"" + r.getY()+"\"},";
+                        html.append(pos).append("\n");
+                    }
+                    String text = html.toString();
+                    text = text.substring(0,text.length()-2)+"\n";
+                    OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream());
+                    out.write("HTTP/1.1 200 OK\r\n");
+                    out.write("Content-Type: application/json\r\n");
+                    out.write("\r\n");
+                    out.write("{\n"+text+"}");
+                    out.flush();
+                    socket.close();
+                }else{
+                    OutputStream output = socket.getOutputStream();
 
-                Thread Client = new Thread(() -> {
-                    boolean dead = false;
-                    char[] preview = new char[8];
-                    BufferedReader in = null;
+                    PrintWriter writer = new PrintWriter(output, true);
 
-                    while (!dead) {
-                        try {
-                            in = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8));
-                            preview = new char[8];
-                            in.mark(8);
-                            in.read(preview, 0, 8);
-                            in.reset();
-                        }catch (SocketException e){
-                            System.out.println("Error: "+e.getMessage());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                    writer.println("Connected");
 
-                        if(Arrays.equals(preview, new char[8])){
-                            dead = true;
-                        }else{
-                            //Read messages
-                            String s = String.valueOf((preview));
-                            String rpin = s.substring(0,4);
-                            if(players.containsKey(rpin)){
-                                System.out.println("Rover "+ rpin + " executed " + s.substring(4,8));
-                                char[] commands = s.substring(4,8).toCharArray();
+                    Thread Client = new Thread(() -> {
+                        boolean dead = false;
+                        char[] preview = new char[8];
+                        BufferedReader in = null;
 
-                                for(char cmd : commands){
-                                    if(Character.isLetter(cmd)) {
-                                        String c = Character.toString(cmd);
-                                        Rover r = players.get(rpin);
-                                        String[] dir = {"N", "E", "S", "W"};
-                                        switch (c) {
-                                            case "s":
-                                                String scan = r.scan(r.getX(), r.getY(), r.getR());
-                                                writer.println(scan);
-                                                writer.flush();
-                                                break;
-                                            case "r":
-                                                r.rRight();
-                                                break;
-                                            case "l":
-                                                r.rLeft();
-                                                break;
-                                            case "p":
-                                                writer.write("Position x:" + r.getX() + " y:" + r.getY() + " direction:" + dir[r.getR()]);
-                                                writer.flush();
-                                                break;
-                                            case "f":
-                                                r.forward();
-                                                break;
-                                            case "X":
-                                                for(Map.Entry<String, Rover> rov : players.entrySet()) {
-                                                    String rover = "Rover "+ rov.getKey();
-                                                    r = rov.getValue();
-                                                    String pos = " Position x:" + r.getX() + " y:" + r.getY() + " direction:" + dir[r.getR()];
-                                                    System.out.println(rover + pos);
-                                                }
-                                                break;
+                        while (!dead) {
+                            try {
+                                in = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8));
+                                preview = new char[8];
+                                in.mark(8);
+                                in.read(preview, 0, 8);
+                                in.reset();
+                            }catch (SocketException e){
+                                System.out.println("Error: "+e.getMessage());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            if(Arrays.equals(preview, new char[8])){
+                                dead = true;
+                            }else{
+                                //Read messages
+                                String s = String.valueOf((preview));
+                                String rpin = s.substring(0,4);
+                                if(players.containsKey(rpin)){
+                                    System.out.println("Rover "+ rpin + " executed " + s.substring(4,8));
+                                    char[] commands = s.substring(4,8).toCharArray();
+
+                                    for(char cmd : commands){
+                                        if(Character.isLetter(cmd)) {
+                                            String c = Character.toString(cmd);
+                                            Rover r = players.get(rpin);
+                                            String[] dir = {"N", "E", "S", "W"};
+                                            switch (c) {
+                                                case "s":
+                                                    String scan = r.scan(r.getX(), r.getY(), r.getR());
+                                                    writer.println(scan);
+                                                    break;
+                                                case "r":
+                                                    r.rRight();
+                                                    break;
+                                                case "l":
+                                                    r.rLeft();
+                                                    break;
+                                                case "p":
+                                                    writer.println("Position x:" + r.getX() + " y:" + r.getY() + " direction:" + dir[r.getR()]);
+                                                    break;
+                                                case "f":
+                                                    r.forward();
+                                                    break;
+                                                case "X":
+                                                    for(Map.Entry<String, Rover> rov : players.entrySet()) {
+                                                        String rover = "Rover "+ rov.getKey();
+                                                        r = rov.getValue();
+                                                        String pos = " Position x:" + r.getX() + " y:" + r.getY() + " direction:" + dir[r.getR()];
+                                                        System.out.println(rover + pos);
+                                                    }
+                                                    break;
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
-                    }
-                    try {
-                        if (in != null) {
-                            in.close();
+                        try {
+                            if (in != null) {
+                                in.close();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-                Client.start();
+                    });
+                    Client.start();
+                }
             }
         } catch (IOException ex) {
             System.out.println("Server exception: " + ex.getMessage());
